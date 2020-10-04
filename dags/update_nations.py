e0fda470ee6f0c3fd40fceadf29c2b2951387a7e
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-10-04 14:12:45 -0700

    Started work on first DAG.

---
+++ dags/update_nations.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from requests import get
+from datetime import datetime, timedelta
+import pandas as pd
+
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2015, 6, 1),
+    "email": ["airflow@airflow.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+dag = DAG('UpdateNations', default_args=default_args, schedule_interval="@daily")
